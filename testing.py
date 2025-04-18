161942c4c7389e09b2ff208eadce4cf3796ff2a7
Kieran-Plenn <kieranplenn@gmail.com>
2025-04-18 10:50:53 -0400

    Tests run but not all pass

--- testing.py
+++ testing.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 import pytest
 from unittest.mock import patch, MagicMock
 from src.tracktidy.batch.processor import batch_process, get_files_for_batch
